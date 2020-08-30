6d2743b5211f58cc37d2d0440b707827d65e3e98
Lee0701 <hgl1002@naver.com>
2020-08-31 04:23:25 +0900

    Add combination test

--- src/test/java/io/github/lee0701/kiturami/ConverterTest.java
+++ src/test/java/io/github/lee0701/kiturami/ConverterTest.java
@@ -5,11 +5,25 @@ import static org.junit.Assert.*;
 import io.github.lee0701.kiturami.converter.Normalize;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConverterTest {
     @Test
     public void testNormalize() {
         Converter<String, String> converter = new Normalize("NFC");
         assertEquals("한글", converter.convert("한글"));
         assertEquals("정규화", converter.convert("정규화"));
+    }
+    @Test
+    public void testCombination() {
+        Map<String, Character> table = new HashMap<>() {{
+            put("ᆸᆺ", 'ᆹ');
+            put("ᆯᆨ", 'ᆰ');
+            put("ᆰᆺ", 'ᇌ');
+        }};
+        Converter<String, String> converter = new Combination(table);
+        assertEquals("없다", converter.convert("업ᆺ다"));
+        assertEquals("아ᇌ", converter.convert("알ᆨᆺ"));
     }
 }
